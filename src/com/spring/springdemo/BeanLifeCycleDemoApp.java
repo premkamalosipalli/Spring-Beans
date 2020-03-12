f2a75b6d829536e1f73837554534d035b4617b4e
premk <premk@192.168.1.11>
2020-03-12 10:17:29 +0530

    Inversion of Control with Annotations

--- src/com/spring/springdemo/BeanLifeCycleDemoApp.java
+++ src/com/spring/springdemo/BeanLifeCycleDemoApp.java
@@ -13,8 +13,14 @@ public class BeanLifeCycleDemoApp {
 		Coach theCoach=context.getBean("myCoach", Coach.class);
 		
 		System.out.println("MemoryLocation to the Coach:"+theCoach);
-
 		
+		//Component Annotation with parameters in tractCoach with@Component("theComponentScan")
+		//Coach theComponentCoach=context.getBean("theComponentScan",Coach.class);
+		//System.out.println("Retrive a Bean Using Component Annotation:"+theComponentCoach.getDailyFortune());
+		
+		//Default Component Annotation without any parameters
+		Coach theDefaultComponentCoach=context.getBean("trackCoach", Coach.class);
+		System.out.println("Retrive a Bean Using Default Component Annotation:"+theDefaultComponentCoach.getDailyFortune());
 		//for prototype scope the destroy dean is not called
 		Coach theCoach1=context.getBean("myCustomCoach", Coach.class);
 		System.out.println("MemoryLocation to the Coach:"+theCoach1);
--- src/com/spring/springdemo/TrackCoach.java
+++ src/com/spring/springdemo/TrackCoach.java
@@ -1,5 +1,8 @@
 package com.spring.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class TrackCoach implements Coach {
 
 	private FortuneService fortuneService;
